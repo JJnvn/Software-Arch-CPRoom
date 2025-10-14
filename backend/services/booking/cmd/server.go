f2982fb48cb199631d7edfa12dcd728fceda5437
Alarm24 <alarmirl24@gmail.com>
2025-10-14 10:07:05 +0700

    addInitialPortForBookingService

--- backend/services/booking/cmd/server.go
+++ backend/services/booking/cmd/server.go
@@ -10,6 +10,7 @@ import (
 	"github.com/JJnvn/Software-Arch-CPRoom/backend/services/booking/models"
 
 	pb "github.com/JJnvn/Software-Arch-CPRoom/backend/services/booking/proto"
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 )
@@ -25,6 +26,22 @@ func main() {
 	// Layers
 	repo := internal.NewBookingRepository(db)
 	service := internal.NewBookingService(repo)
+	handler := internal.NewBookingHandler(service)
+
+	httpPort := os.Getenv("BOOKING_HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8083"
+	}
+
+	go func() {
+		app := fiber.New()
+		app.Post("/bookings", handler.CreateBooking)
+
+		log.Printf("Booking HTTP server running on :%s", httpPort)
+		if err := app.Listen(":" + httpPort); err != nil {
+			log.Fatalf("failed to start booking HTTP server: %v", err)
+		}
+	}()
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterBookingServiceServer(grpcServer, service)
